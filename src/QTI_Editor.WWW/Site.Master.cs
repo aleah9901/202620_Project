5d12c35172bea123f913bbb1fa2da5df2e88bd3f
inkbones <liamvmalone@gmail.com>
2026-03-18 15:29:27 -0500

    still trying to make writelines work

--- src/QTI_Editor.WWW/Site.Master.cs
+++ src/QTI_Editor.WWW/Site.Master.cs
@@ -6,17 +6,20 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+
+
 namespace QTI_Editor.WWW
 {
+
     public partial class Site : System.Web.UI.MasterPage
     {
-        
         protected void Page_Load(object sender, EventArgs e)
         {
             // had to create a directory. if this works how I think it works it should be created when the site first loads
 
             //I'm gonna do some writelines to see if it actually works
             Console.WriteLine("Hello World!!!!!!!!!!!!!!!!!!!!1");
+
             string direc = @"C:\direc";
             Directory.CreateDirectory(direc);
 
