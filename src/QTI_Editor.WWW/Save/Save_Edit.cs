68946e0bb054478c3828a20780bf7a2bce10b0e8
StephenHJSCC <A01891748@alabama.edu>
2026-03-24 04:34:31 -0500

    Added class structure of Save_Edit, Save_Zip, and Sesssion_clean. Added logic for Session_Clean.

--- src/QTI_Editor.WWW/Save/Save_Edit.cs
+++ src/QTI_Editor.WWW/Save/Save_Edit.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+using System.IO;
+
+namespace QTI_Editor.WWW
+{
+    //This class will allow us to save individual question edits.
+    public static class SaveEditService
+    {
+        public static void SaveEdit()
+        {
+
+        }
+    }
+}
--- src/QTI_Editor.WWW/Save/Save_Zip.cs
+++ src/QTI_Editor.WWW/Save/Save_Zip.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+using System.IO;
+
+//This class will build a ZIP file that saves the user edits
+namespace QTI_Editor.WWW
+{
+    public static class SaveZipService
+    {
+        public static string BuildZip()
+        {
+
+        }
+    }
+}
