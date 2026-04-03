bbbe7b84454bc3213ee9e67c95d5eacac2ae4ddb
KaaisT05 <A01368185@alabama.edu>
2026-04-03 17:38:29 -0500

    Save to directory

--- src/QTI_Editor.WWW/Save/Save_Edit.cs
+++ src/QTI_Editor.WWW/Save/Save_Edit.cs
@@ -6,8 +6,25 @@ namespace QTI_Editor.WWW
     //This class will allow us to save individual question edits.
     public static class SaveEditService
     {
-        public static void SaveEdit()
+        public static void SaveEdit(string edits )
         {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "edits.txt");
+
+            File.WriteAllText(filePath, edits);
+            Console.WriteLine("Edits Saved!");
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         }
     }
